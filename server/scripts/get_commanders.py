763f983bbcf0830edd0aba4fc2bec45451a3e4d2
Jason Qiu <jqiu21@protonmail.com>
2022-12-29 12:39:44 -0500

    Add commander metadata script

---
+++ server/scripts/get_commanders.py
@@ -0,0 +1,55 @@
+from pymongo import MongoClient
+import mtg_api # Import from file
+from functools import reduce
+from tqdm import tqdm
+
+client = MongoClient("mongodb://localhost:27017")
+
+db = client['test']
+
+collections = ['test']#, 'mox_masters']
+
+def wubrgify(color_string):
+    """Parse colors in WUBRG(C) order; removes duplicates and misordering"""
+    res = ''
+    for c in ['W', 'U', 'B', 'R', 'G']:
+        res += c if c in color_string else ''
+    return res if res else "C" # return "C" for colorless if W, U, B, R, G all not in color string
+
+for c in collections:
+    print(f"Updating Collection {c} with commander/color identity metadata")
+    col = client['test'][c]
+    for i in tqdm(col.find()):
+        if 'Commander' in i.keys() and 'Color_ID' in i.keys(): # Metadata already exists
+            continue
+        try:
+            decklist_url = i['Decklist']
+        except KeyError:
+            print("Warning: no decklist url") # TODO: provide more useful logging data here
+            continue
+
+        try:
+            commanders = mtg_api.get_deck(decklist_url).get_commander()
+        except KeyError:
+            print("Warning: error while fetching decklist.") # TODO: provide more useful logging data here  
+            continue
+        
+        # Get single string for name(s) of commander(s)
+        if len(commanders) == 1:
+            commander_string = commanders[0]
+        elif len(commanders) == 2: # Partners, Friends Forever, Background, whatever Wizards plans to do next
+            commander_string = commanders[0] + ' / ' + commanders[1]
+        else:
+            print("Warning: Number of commanders is not 1 or 2. Skipped.") # TODO: provide more useful logging data here
+            continue
+        
+        # Get color identity for commander(s)
+        color_id = ''
+        for commander in commanders:
+            colors = mtg_api.get_card(commander)['color_identity']
+            color_id = color_id + reduce(lambda x, y: x + y, colors) if colors else ""
+        color_id = wubrgify(color_id)
+    
+        # print(commander_string, '')
+        # print(color_id)
+        col.update_one(i, {'$set': {'Commander': commander_string, 'Color_ID': color_id}})
