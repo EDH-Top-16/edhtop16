94caf9bcbe7e166581e69e07bffae2516304d038
g00gol <jackeyyang.work@gmail.com>
2023-01-05 17:20:45 -0500

    Created banner, searchbar, and edited deckview @TODO need to get API endpoint that takes the current input and suggests results

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -1,3 +1,5 @@
+import Searchbar from "./Searchbar";
+
 export default function Banner({
   title,
   enableSearchbar,
@@ -6,8 +8,8 @@ export default function Banner({
 }) {
   return (
     <div className="relative w-full h-full bg-banner top-16 px-24 py-12">
-      <h1>{title}</h1>
-      {enableSearchbar ? <div></div> : <></>}
+      <h1 className="text-3xl font-bold text-text">{title}</h1>
+      <div className="mt-12">{enableSearchbar ? <Searchbar /> : <></>}</div>
     </div>
   );
 }
--- client/src/components/DeckView.js
+++ client/src/components/DeckView.js
@@ -1,4 +1,4 @@
-import Banner from "./Banner";
+import Banner from "./Banner/Banner";
 
 export default function DeckView() {
   return (
