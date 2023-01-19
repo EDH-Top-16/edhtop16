33cbaab8618bae1d39a3beed1baec2d6b89eed85
Jason Qiu <jqiu21@protonmail.com>
2023-01-19 10:56:49 -0500

    Add request to getCommanders

--- client/src/components/CommanderView/CommanderView.js
+++ client/src/components/CommanderView/CommanderView.js
@@ -1,7 +1,13 @@
 import Banner from "../Banner/Banner";
 import Entry from "./Entry";
+import axios from "axios";
 
 export default function CommanderView() {
+  const config = {
+    headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}
+  };
+  const data = {};
+  axios.post(process.env.REACT_APP_uri + "/api/req", data, config).then(function(res){console.log(res);});
   return (
     <div className="flex flex-col w-11/12 ml-auto mr-0">
       <Banner title={"View Decks"} enableSearchbar={true} enableColors={true} />
