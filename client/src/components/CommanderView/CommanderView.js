8576cb0b144272479c7e70596c9609a79aead8e5
g00gol <jackeyyang.work@gmail.com>
2023-01-23 12:45:19 -0500

    initialized react-router-dom but bugged

--- client/src/components/CommanderView/CommanderView.js
+++ client/src/components/CommanderView/CommanderView.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Link } from "react-router-dom";
 
 import Banner from "../Banner/Banner";
 import Entry from "./Entry";
@@ -27,49 +28,57 @@ export default function CommanderView() {
   }
 
   return (
-    <div className="flex flex-col w-11/12 ml-auto mr-0">
-      {/* Banner */}
-      <Banner
-        title={"View Decks"}
-        enableSearchbar={true}
-        enableColors={true}
-        enableFilters={true}
-      />
+    <BrowserRouter>
+      <div className="flex flex-col w-11/12 ml-auto mr-0">
+        {/* Banner */}
+        <Banner
+          title={"View Decks"}
+          enableSearchbar={true}
+          enableColors={true}
+          enableFilters={true}
+        />
 
-      {/* Table of commanders */}
-      <table className="block mx-24 my-12 table-fixed">
-        <tbody className="[&>tr]:space-y-6 [&>tr>td]:w-max [&>tr>td]:px-2 [&>tr>td]:py-4">
-          <tr className="text-subtext text-lg underline">
-            <td>#</td>
-            <td>Name</td>
-            <td className="cursor-pointer" onClick={() => changeTopX()}>
-              Top {topX}s
-            </td>
-            <td>Entries</td>
-            <td>Conversion</td>
-            <td>Colors</td>
-          </tr>
-          {isLoading ? (
-            <tr>Loading...</tr>
-          ) : (
-            commanders &&
-            commanders.map((k, v) => (
-              <Entry
-                rank={v + 1}
-                name={commanders[v].commander}
-                metadata={[
-                  commanders[v].topX,
-                  commanders[v].count,
-                  ((commanders[v].topX / commanders[v].count) * 100).toFixed(
-                    2
-                  ) + "%",
-                ]}
-                colors={commanders[v].colorID}
-              />
-            ))
-          )}
-        </tbody>
-      </table>
-    </div>
+        {/* Table of commanders */}
+        <table className="block mx-24 my-12 table-fixed">
+          <tbody className="[&>tr]:space-y-6 [&>tr>td]:w-max [&>tr>td]:px-2 [&>tr>td]:py-4">
+            <tr className="text-subtext text-lg underline">
+              <td>#</td>
+              <td>Name</td>
+              <td className="cursor-pointer" onClick={() => changeTopX()}>
+                Top {topX}s
+              </td>
+              <td>Entries</td>
+              <td>Conversion</td>
+              <td>Colors</td>
+            </tr>
+            {isLoading ? (
+              <tr>Loading...</tr>
+            ) : (
+              commanders &&
+              commanders.map((k, v) => (
+                <Routes
+                  path={`/commander/${commanders[v].commander}`}
+                  component={
+                    <Entry
+                      rank={v + 1}
+                      name={commanders[v].commander}
+                      metadata={[
+                        commanders[v].topX,
+                        commanders[v].count,
+                        (
+                          (commanders[v].topX / commanders[v].count) *
+                          100
+                        ).toFixed(2) + "%",
+                      ]}
+                      colors={commanders[v].colorID}
+                    />
+                  }
+                />
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+    </BrowserRouter>
   );
 }
