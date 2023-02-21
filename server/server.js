af9acea655c58b6a1d93a269d2f3b0a276048bda
Jason Qiu <jqiu21@protonmail.com>
2023-02-21 16:54:48 -0500

    Add rate limiiting to API

--- server/server.js
+++ server/server.js
@@ -3,12 +3,17 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+
+const { rateLimiter } = require("./middleware/rate_limit");
+
+app.use(rateLimiter);
+
 app.use(cors());
 app.use(express.json({strict: false}));
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
- 
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
