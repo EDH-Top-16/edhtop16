f2d5db9293bbca1b4bc4e6f9105b5dd8785d664e
Jason Qiu <jqiu21@protonmail.com>
2023-02-21 17:22:53 -0500

    Increase rate limit

--- server/middleware/rate_limit.js
+++ server/middleware/rate_limit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
     windowMS: 60 * 1000, // 1 min
-    max: 10, // TODO: figure out what this number should be.
+    max: 120, // TODO: figure out what this number should be.
     message: 'Too many requests. Please try again in 1 min.',
     statusCode: 429,
     standardHeaders: true,
