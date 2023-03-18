86261f3675fdf25eb2284b3800d45dde86b8e796
g00gol <jackey2870@gmail.com>
2023-03-17 22:31:41 -0400

    404 page implemented and route checking

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,52 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import Nav from "./components/Nav";
 import CommanderView from "./components/CommanderView/CommanderView";
 import DeckView from "./components/DeckView/DeckView";
 import TournamentView from "./components/TournamentView/TournamentView";
 import APIDocs from "./components/APIDocs/APIDocs";
+import NotFound from "./components/404";
 
 /**
  * @TODO make a list of valid URLs so commander/asdf isn't an actual page lol
  */
 
 function App() {
+  const [commander, setCommander] = useState("");
+  const [commanderExist, setCommanderExist] = useState(true);
+
+  // useEffect to get commanders
+  useEffect(() => {
+    axios
+      .post(process.env.REACT_APP_uri + "/api/req", { commander: commander })
+      .then((res) => {
+        // console.log(commander, res.data);
+        if (res.data.length > 0) {
+          setCommanderExist(true);
+        } else {
+          setCommanderExist(false);
+        }
+      });
+  }, [commander]);
+
   return (
     <div className="bg-bg_primary min-w-screen min-h-screen">
       <BrowserRouter>
         <Nav />
         <Routes>
           <Route index element={<CommanderView />} />
-          <Route path="commander/*" element={<DeckView />} />
+          <Route
+            path="commander/*"
+            element={
+              commanderExist ? (
+                <DeckView setCommander={setCommander} />
+              ) : (
+                <NotFound />
+              )
+            }
+          />
           <Route path="api" element={<APIDocs />} />
           <Route path="tournament" element={<TournamentView />} />
         </Routes>
---
+++ client/src/components/404.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="w-11/12 ml-auto mr-0 h-screen flex flex-col justify-center items-center text-text">
+      <h1 className="text-4xl font-bold text-center">404</h1>
+      <p className="text-center text-2xl">
+        Page Not Found. Please check the URL and try again.
+      </p>
+    </div>
+  );
+}
