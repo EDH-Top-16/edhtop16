8d1b4970e38c829873a0967ced4c376bb3c89641
g00gol <jackey2870@gmail.com>
2023-09-08 22:38:44 -0400

    added key for colors

--- client/src/components/Banner/filters/colors.tsx
+++ client/src/components/Banner/filters/colors.tsx
@@ -5,8 +5,10 @@ export default function Colors(): JSX.Element {
   return (
     <BaseFilter title={"Colors"} tag={"colors"}>
       <div className="grid grid-cols-3 gap-4">
-        {Object.values(allColors).map((color) => (
-          <button className="[&>svg]:w-10">{color}</button>
+        {Object.values(allColors).map((color, i) => (
+          <button key={i} className="[&>svg]:w-10">
+            {color}
+          </button>
         ))}
       </div>
     </BaseFilter>
