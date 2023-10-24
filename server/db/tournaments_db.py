deb377953184335c253c5da5b7aea28dcab8e916
Ryan Delaney <rrdelaney@outlook.com>
2023-10-23 17:21:35 -0700

    Fix Python server type checking

--- server/db/tournaments_db.py
+++ server/db/tournaments_db.py
@@ -1,9 +1,9 @@
-from typing import List
+from typing import List, Any
 import logging
 
 from .base_db import get_db_async
 from utils import oIdToString
-from utils.types import AllFilters, TournamentFilters, Tournament, Commander
+from utils.types import AllFilters, TournamentFilters, Tournament, Commander, Entry
 
 
 async def get_tournaments(tournament_filters: TournamentFilters) -> List[Tournament]:
@@ -13,8 +13,9 @@ async def get_tournaments(tournament_filters: TournamentFilters) -> List[Tournam
     db = await get_db_async("cedhtop16")
     metadata = db["metadata"]
 
-    result = await metadata.find(tournament_filters, {"_id": 0}).to_list(length=None)
-    return result
+    db_filters = tournament_filters.model_dump(by_alias=True, exclude_unset=True, exclude_none=True)
+    result: List[Any] = await metadata.find(db_filters, {"_id": 0}).to_list(length=10000) # type: ignore
+    return [Tournament(**t) for t in result]
 
 
 async def get_commanders(filters: AllFilters) -> dict[str, Commander]:
@@ -26,7 +27,7 @@ async def get_commanders(filters: AllFilters) -> dict[str, Commander]:
     commanders: dict[str, Commander] = {}
     for player in players:
         # For each player, check if the commander is in the dictionary.
-        commander = player.get("commander", "")
+        commander = player.commander or ""
         if not commander:
             logging.warning(f"Can't get commander for player: {player}")
             continue
@@ -37,86 +38,88 @@ async def get_commanders(filters: AllFilters) -> dict[str, Commander]:
 
         if commander not in commanders:
             # If the commander is not in the dictionary, add it.
-            commanders[commander] = {
-                "colorID": player.get("colorID", "") or "",
-                "wins": player.get("wins", 0) or 0,
-                "winsSwiss": player.get("winsSwiss", 0) or 0,
-                "winsBracket": player.get("winsBracket", 0) or 0,
-                "draws": player.get("draws", 0) or 0,
-                "losses": player.get("losses", 0) or 0,
-                "lossesSwiss": player.get("lossesSwiss", 0) or 0,
-                "lossesBracket": player.get("lossesBracket", 0) or 0,
-                "topCuts": 1 if player.get("standing", float('inf')) <= player.get("topCut", 0) else 0,
-                "count": 1
-            }
+            commanders[commander] = Commander(
+                colorID= player.colorID or "",
+                wins= player.wins or 0,
+                winsSwiss= player.winsSwiss or 0,
+                winsBracket= player.winsBracket or 0,
+                draws= player.draws or 0,
+                losses= player.losses or 0,
+                lossesSwiss= player.lossesSwiss or 0,
+                lossesBracket= player.lossesBracket or 0,
+                topCuts= 1 if (player.standing or float('inf')) <= (player.topCut or 0) else 0,
+                count= 1
+            )
         else:
             # If the commander is in the dictionary, update it.
-            c = commanders.get(commander, {})
-            if c == {}:
+            c = commanders.get(commander)
+            if c is None:
                 logging.warning(f"Malformed commander: {commander}")
                 continue
 
             # If the colorID is not set, set it if this player has a valid colorID.
-            if player.get("colorID", "") in {"", "N/A"} and not c["colorID"]:
-                c["colorID"] = player["colorID"]
+            if player.colorID in {"", "N/A"} and not c.colorID:
+                c.colorID = player.colorID
 
             # Update counting stat fields
-            fields = ["wins", "winsSwiss", "winsBracket",
-                      "draws", "losses", "lossesSwiss", "lossesBracket"]
-            for field in fields:
-                c[field] += player.get(field, 0) or 0
+            c.wins = (c.wins or 0)  + (player.wins or 0)
+            c.winsSwiss = (c.winsSwiss or 0)  + (player.winsSwiss or 0)
+            c.winsBracket = (c.winsBracket or 0)  + (player.winsBracket or 0)
+            c.draws = (c.draws or 0)  + (player.draws or 0)
+            c.losses = (c.losses or 0)  + (player.losses or 0)
+            c.lossesSwiss = (c.lossesSwiss or 0)  + (player.lossesSwiss or 0)
+            c.lossesBracket = (c.lossesBracket or 0)  + (player.lossesBracket or 0)
 
-            if player.get("standing", float('inf')) <= player.get("topCut", 0):
-                c["topCuts"] += 1
+            if (player.standing or float('inf')) <= (player.topCut or 0):
+                c.topCuts = (c.topCuts or 0) + 1
             # Increment counter
-            c["count"] += 1
+            c.count = (c.count or 0)+ 1
 
     # Calculate the winRate, winRateSwiss, and winRateBracket
     for commander in commanders:
         c = commanders[commander]
-        c["winRate"] = c["wins"] / (c["wins"] + c["losses"] + c["draws"]) \
-            if (c["wins"] + c["losses"] + c["draws"]) > 0 else None
-        c["winRateSwiss"] = c["winsSwiss"] / \
-            (c["winsSwiss"] + c["lossesSwiss"] + c["draws"]) \
-            if (c["winsSwiss"] + c["lossesSwiss"] + c["draws"]) > 0 else None
-        c["winRateBracket"] = c["winsBracket"] / (c["wins"] + c["losses"]) \
-            if (c["wins"] + c["losses"]) > 0 else None
-        c["conversionRate"] = c["topCuts"] / c["count"]
+        c.winRate = (c.wins or 0) / ((c.wins or 0) + (c.losses or 0) + (c.draws or 0)) \
+            if ((c.wins or 0) + (c.losses or 0) + (c.draws or 0)) > 0 else None
+        c.winRateSwiss = (c.winsSwiss or 0) / \
+            ((c.winsSwiss or 0) + (c.lossesSwiss or 0) + (c.draws or 0)) \
+            if ((c.winsSwiss or 0) + (c.lossesSwiss or 0) + (c.draws or 0)) > 0 else None
+        c.winRateBracket = (c.winsBracket or 0) / ((c.wins or 0) + (c.losses or 0)) \
+            if ((c.wins or 0) + (c.losses or 0)) > 0 else None
+        c.conversionRate = (c.topCuts or 0) / (c.count or 0)
 
     return commanders
 
 
-async def get_entries(filters: AllFilters) -> List[dict]:
+async def get_entries(filters: AllFilters) -> List[Entry]:
     """
     Returns a list of players from the database.
     """
     db = await get_db_async("cedhtop16")
 
-    tournament_filters = filters.get("tournament_filters", {})
+    tournament_filters = filters.tournament_filters or TournamentFilters()
     tournaments = await get_tournaments(tournament_filters)
-    if "tournament_filters" in filters:
-        del filters["tournament_filters"]
+    if filters.tournament_filters is not None:
+        filters.tournament_filters = None
 
-    res: List[dict] = []
+    res: List[Entry] = []
     for tournament in tournaments:
         # Get the TID from the metadata document
-        tid = tournament["TID"]
-        if tid == "":
+        tid = tournament.TID
+        if tid is None or tid == "":
             logging.warning(f"Empty TID for tournament: {tournament}")
             continue
 
         # Get the tournament with matching TID from the database
         t = db[tid]
-        if t is None:
-            logging.warning(f"Could not find tournament: {tid}")
-            continue
 
         # Get the tournament data from the database
-        result = await t.find(filters).to_list(length=None)
+        db_filters = filters.model_dump(by_alias=True, exclude_unset=True, exclude_none=True)
+        result: List[Any] = await t.find(db_filters).to_list(length=10000) # type: ignore
         for i in result:
             i['TID'] = tid
-            i['tournamentName'] = tournament.get('tournamentName', "")
-            i['topCut'] = tournament.get('topCut', 0)   
-        res.extend(result)
+            i['tournamentName'] = tournament.tournamentName
+            i['topCut'] = tournament.topCut or 0   
+
+        res.extend([Entry(**e) for e in result])
 
     return oIdToString(res)
