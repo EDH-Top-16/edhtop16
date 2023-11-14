9cc9bc5d0b65605fb946a1b66d7998b0a2b3cf16
Ryan Delaney <rrdelaney@outlook.com>
2023-11-13 20:39:49 -0800

    Adds a `Dockerfile` to run both the Next.js frontend app and the Python backend app using Nginx Unit.
    Nginx unit

---
+++ client/server.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+/**
+ * @fileoverview This is a custom Next.js server only intended for use in
+ * production. It overrides Next.js's usage of the http module to integrate
+ * with unit-http, but otherwise adds no additional behavior.
+ */
+
+const { createServer } = require("unit-http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+
+// These variables are overridden at runtime by unit-http.
+const hostname = "localhost";
+const port = 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("internal server error");
+    }
+  })
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
