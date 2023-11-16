d3b5ad15b5c4efb18511587d3a0d876774374d9c
Dan Mudd <dan@danmudd.io>
2023-11-16 22:29:11 +0000

    Run prettier

--- client/src/components/csvExport.tsx
+++ client/src/components/csvExport.tsx
@@ -1,38 +1,44 @@
-
-import React, { useContext } from 'react';
-import { StreamParser } from '@json2csv/plainjs';
-import { saveAs } from 'file-saver';
-import moment from 'moment';
-import { FilterContext } from '../context/filter';
-import { AllFiltersType } from '../utils/types/filters';
-import { getCommanders } from '../api/commander';
+import React, { useContext } from "react";
+import { StreamParser } from "@json2csv/plainjs";
+import { saveAs } from "file-saver";
+import moment from "moment";
+import { FilterContext } from "../context/filter";
+import { AllFiltersType } from "../utils/types/filters";
+import { getCommanders } from "../api/commander";
 
 const ExportCSVButton = (): JSX.Element => {
-    const { filters } = useContext(FilterContext);
+  const { filters } = useContext(FilterContext);
 
-    return (<button className="filter-btn" onClick={() => downloadDecksAsCsv(filters)}>Export to CSV</button>)
-}
+  return (
+    <button className="filter-btn" onClick={() => downloadDecksAsCsv(filters)}>
+      Export to CSV
+    </button>
+  );
+};
 
 const downloadDecksAsCsv = async (filters: AllFiltersType) => {
-    const opts = {};
-    const asyncOpts = {};
-    const parser = new StreamParser(opts, asyncOpts);
+  const opts = {};
+  const asyncOpts = {};
+  const parser = new StreamParser(opts, asyncOpts);
 
-    let csv = '';
-    parser.onData = (chunk) => (csv += chunk.toString());
-    parser.onEnd = () => {
-        const blob = new Blob([csv], { type: 'text/csv' });
-        saveAs(blob, `edhtop16-export-${moment().format('YYYY-MM-DD_HH-mm-ss')}.csv`);
-    };
-    parser.onError = (err) => console.error(err);
+  let csv = "";
+  parser.onData = (chunk) => (csv += chunk.toString());
+  parser.onEnd = () => {
+    const blob = new Blob([csv], { type: "text/csv" });
+    saveAs(
+      blob,
+      `edhtop16-export-${moment().format("YYYY-MM-DD_HH-mm-ss")}.csv`,
+    );
+  };
+  parser.onError = (err) => console.error(err);
 
-    const commanders = getCommanders(filters).then(data => {
-        Object.entries(data).forEach(([key, value]) => {
-            parser.pushLine({ name: key, ...value })
-        })
-
-        parser.end();
+  const commanders = getCommanders(filters).then((data) => {
+    Object.entries(data).forEach(([key, value]) => {
+      parser.pushLine({ name: key, ...value });
     });
-}
+
+    parser.end();
+  });
+};
 
 export default ExportCSVButton;
