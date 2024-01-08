c6c0e0423688aa8ec0b30f05f23c741f9179daed
Jason Qiu <jqiu21@protonmail.com>
2024-01-08 15:36:48 -0500

    Format docuemnt

--- server/scripts/3nf.py
+++ server/scripts/3nf.py
@@ -33,7 +33,6 @@ if __name__ == "__main__":
                 )
 
         for entry in db_1nf[tournament["TID"]].find():
-
             # Add player to collection if does not exist
             player_id = None
             try:
@@ -44,50 +43,92 @@ if __name__ == "__main__":
                         {"name": entry["name"], "profile": entry["profile"]},
                         {"name": entry["name"], "profile": entry["profile"]},
                         upsert=True,
-                        return_document=pymongo.collection.ReturnDocument.AFTER
+                        return_document=pymongo.collection.ReturnDocument.AFTER,
                     )["_id"]
-                elif db_3nf["players"].count_documents({"name": entry["name"], "profile": entry["profile"]}) < 1:
-                    player_id = db_3nf["players"].insert_one({"name": entry["name"], "profile": entry["profile"]}).inserted_id
+                elif (
+                    db_3nf["players"].count_documents(
+                        {"name": entry["name"], "profile": entry["profile"]}
+                    )
+                    < 1
+                ):
+                    player_id = (
+                        db_3nf["players"]
+                        .insert_one(
+                            {"name": entry["name"], "profile": entry["profile"]}
+                        )
+                        .inserted_id
+                    )
                 else:
-                    player_id = db_3nf["players"].find_one({"name": entry["name"], "profile": entry["profile"]})["_id"]
+                    player_id = db_3nf["players"].find_one(
+                        {"name": entry["name"], "profile": entry["profile"]}
+                    )["_id"]
             except Exception as e:
-                print(f"Error while processing player -- tournament: {tournament['TID']}     entry: {entry['_id']}     error: {e}")
+                print(
+                    f"Error while processing player -- tournament: {tournament['TID']}     entry: {entry['_id']}     error: {e}"
+                )
 
             # Add commander to collection if does not exist
             commander_id = None
             try:
                 if "commander" not in entry.keys():
-                    print(f"Warning: no commander for -- tournament: {tournament['TID']}     entry: {entry['_id']}")
+                    print(
+                        f"Warning: no commander for -- tournament: {tournament['TID']}     entry: {entry['_id']}"
+                    )
                 elif overwrite_data:
                     commander_id = db_3nf["commanders"].find_one_and_update(
                         {"name": entry["commander"]},
                         {"name": entry["commander"], "colorID": entry["colorID"]},
                         upsert=True,
-                        return_document=pymongo.collection.ReturnDocument.AFTER
+                        return_document=pymongo.collection.ReturnDocument.AFTER,
                     )["_id"]
-                elif db_3nf["commanders"].count_documents({"name": entry["commander"]}) < 1:
-                    commander_id = db_3nf["commanders"].insert_one({"name": entry["commander"], "colorID": entry["colorID"]}).inserted_id
+                elif (
+                    db_3nf["commanders"].count_documents({"name": entry["commander"]})
+                    < 1
+                ):
+                    commander_id = (
+                        db_3nf["commanders"]
+                        .insert_one(
+                            {"name": entry["commander"], "colorID": entry["colorID"]}
+                        )
+                        .inserted_id
+                    )
                 else:
-                    commander_id = db_3nf["commanders"].find_one({"name": entry["commander"]})["_id"]
+                    commander_id = db_3nf["commanders"].find_one(
+                        {"name": entry["commander"]}
+                    )["_id"]
             except Exception as e:
-                print(f"Error while processing commander -- tournament: {tournament['TID']}     entry: {entry['_id']}     error: {e}")
+                print(
+                    f"Error while processing commander -- tournament: {tournament['TID']}     entry: {entry['_id']}     error: {e}"
+                )
             # TODO: Add/update entries collection with entries information
             try:
                 if "decklist" not in entry:
                     entry["decklist"] = None
-                if overwrite_data or db_3nf["entries"].count_documents({"playerID": player_id, "commanderID": commander_id}) < 1:
+                if (
+                    overwrite_data
+                    or db_3nf["entries"].count_documents(
+                        {"playerID": player_id, "commanderID": commander_id}
+                    )
+                    < 1
+                ):
                     db_3nf["entries"].find_one_and_update(
                         {"playerID": player_id, "commanderID": commander_id},
-                        {"$set": {"playerID": player_id,
-                        "commanderID": commander_id,
-                        "standing": entry["standing"],
-                        "decklist": entry["decklist"],
-                        "winsSwiss": entry["winsSwiss"] or 0,
-                        "winsBracket": entry["winsBracket"] or 0,
-                        "draws": entry["draws"] or 0,
-                        "lossesSwiss": entry["lossesSwiss"] or 0,
-                        "lossesBracket": entry["lossesBracket"] or 0}},
-                        upsert=True
+                        {
+                            "$set": {
+                                "playerID": player_id,
+                                "commanderID": commander_id,
+                                "standing": entry["standing"],
+                                "decklist": entry["decklist"],
+                                "winsSwiss": entry["winsSwiss"] or 0,
+                                "winsBracket": entry["winsBracket"] or 0,
+                                "draws": entry["draws"] or 0,
+                                "lossesSwiss": entry["lossesSwiss"] or 0,
+                                "lossesBracket": entry["lossesBracket"] or 0,
+                            }
+                        },
+                        upsert=True,
                     )
             except Exception as e:
-                print(f"Error while processing entry -- tournament: {tournament['TID']}     entry: {entry['_id']}     error: {e}")
+                print(
+                    f"Error while processing entry -- tournament: {tournament['TID']}     entry: {entry['_id']}     error: {e}"
+                )
