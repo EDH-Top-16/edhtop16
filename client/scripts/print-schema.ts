358955027c8ffd7804ac1122660de16029aaa90e
Ryan Delaney <rrdelaney@outlook.com>
2024-02-26 23:01:03 -0800

    Format GraphQL schema before printing

--- client/scripts/print-schema.ts
+++ client/scripts/print-schema.ts
@@ -1,6 +1,10 @@
 import { writeFileSync } from "fs";
 import { lexicographicSortSchema, printSchema } from "graphql";
 import { schema } from "../src/lib/server/schema";
+import { format } from "prettier";
 
-const schemaAsString = printSchema(lexicographicSortSchema(schema));
-writeFileSync("src/queries/schema.graphql", schemaAsString);
+format(printSchema(lexicographicSortSchema(schema)), {
+  parser: "graphql",
+}).then((schemaAsString) => {
+  writeFileSync("src/queries/schema.graphql", schemaAsString);
+});
