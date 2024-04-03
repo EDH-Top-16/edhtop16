7574e08b509e6934003ec47c83d70713a26b87c2
Ryan Delaney <rrdelaney@outlook.com>
2024-04-02 22:58:12 -0700

    Rewrite /v2/api/graphql to /api/graphql

--- client/next.config.js
+++ client/next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         source: "/v2",
         destination: "/v2/commanders",
       },
+      {
+        source: "/v2/api/graphql",
+        destination: "/api/graphql",
+      },
     ];
   },
   compiler: { relay: require("./relay.config") },
