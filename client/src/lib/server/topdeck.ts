298a758efd6aec8b28cc962e1a022692f54f1c84
Ryan Delaney <rrdelaney@outlook.com>
2024-05-20 18:31:49 -0700

    Add rounds data from topdeck API

---
+++ client/src/lib/server/topdeck.ts
@@ -0,0 +1,78 @@
+import DataLoader from "dataloader";
+import { z } from "zod";
+
+export type TopdeckTournamentTable = z.infer<
+  typeof topdeckTournamentTableSchema
+>;
+const topdeckTournamentTableSchema = z.object({
+  table: z.number().int(),
+  players: z.array(
+    z.object({
+      id: z.string(),
+      name: z.string(),
+    }),
+  ),
+  winner: z.string().optional(),
+  status: z.string(),
+});
+
+export type TopdeckTournamentRound = z.infer<
+  typeof topdeckTournamentRoundSchema
+>;
+const topdeckTournamentRoundSchema = z.object({
+  round: z.union([z.string(), z.number()]),
+  tables: z.array(topdeckTournamentTableSchema),
+});
+
+export type TopdeckTournament = z.infer<typeof topdeckTournamentSchema>;
+const topdeckTournamentSchema = z.object({
+  TID: z.string(),
+  rounds: z.array(topdeckTournamentRoundSchema),
+  tournamentName: z.string(),
+  swissNum: z.number().int(),
+  startDate: z.number().int(),
+  topCut: z.number().int(),
+});
+
+export class TopdeckClient {
+  private readonly roundsLoader = new DataLoader(
+    async (tournmentIds: readonly string[]) => {
+      const res = await fetch("https://topdeck.gg/api/v2/tournaments", {
+        method: "POST",
+        headers: {
+          Authorization: this.apiKey,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          TID: tournmentIds,
+          rounds: true,
+          players: ["id", "name"],
+          columns: [],
+        }),
+      });
+
+      if (!res.ok) {
+        throw new Error(`Error loading tournaments: ${await res.text()}`);
+      }
+
+      const tournaments = z
+        .array(topdeckTournamentSchema)
+        .parse(await res.json());
+
+      const tournamentsById = new Map(tournaments.map((t) => [t.TID, t]));
+
+      return tournmentIds.map((tid) => tournamentsById.get(tid));
+    },
+  );
+
+  constructor(private readonly apiKey = process.env.TOPDECK_GG_API_KEY!) {
+    if (!this.apiKey) {
+      throw new Error("Must provide `apiKey` or set TOPDECK_GG_API_KEY");
+    }
+  }
+
+  loadRoundsData(tid: string) {
+    return this.roundsLoader.load(tid);
+  }
+}
