296810817085812807dd63420d48f2e546cdbdc4
Jason Qiu <jqiu21@protonmail.com>
2024-06-13 15:53:23 -0400

    More syntax error

--- server/scripts/get_tournaments.py
+++ server/scripts/get_tournaments.py
@@ -61,6 +61,7 @@ def fetch_tournaments(filters=None):
 
 
 if __name__ == "__main__":
+    print(dotenv_values("./config.env"))
     client = MongoClient(dotenv_values("./config.env")["ATLAS_URI"])
 
     db = client["cedhtop16"]
