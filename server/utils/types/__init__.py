c2a96fc9a19914f45804b9aa075641a2b9fe6402
Ryan Delaney <rrdelaney@outlook.com>
2024-09-05 23:31:42 -0700

    Remove unused server code (#103)
    * Remove unused server code
    
    * Fix actions config

--- server/utils/types/__init__.py
+++
@@ -1,6 +0,0 @@
-# pyright: reportUnusedImport=false
-from .filters import AllFilters, BaseFilters, TournamentFilters, OperatorType
-from .tournaments import Tournament
-from .commanders import Commander
-from .players import Player
-from .entries import Entry, DBEntry
--- server/utils/wubrgify.py
+++
@@ -1,8 +0,0 @@
-def wubrgify(color_string):
-    """Parse colors in WUBRG(C) order; removes duplicates and misordering"""
-    res = ""
-    for c in ["W", "U", "B", "R", "G"]:
-        res += c if c in color_string else ""
-    return (
-        res if res else "C"
-    )  # return "C" for colorless if W, U, B, R, G all not in color string
