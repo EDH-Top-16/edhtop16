6ae5ffb54672d6475060c0bcab42493ef2044f30
Ryan Delaney <rrdelaney@outlook.com>
2024-09-16 19:51:37 -0700

    Fix padding on mobile placeholder (#111)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/client/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/assets/**/*.{js,ts,jsx,tsx,mdx}",
   ],
