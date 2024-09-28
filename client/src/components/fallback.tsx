ef949e5a0e8bdc25e0588712c674a6ab0fb6de44
Ryan Delaney <rrdelaney@outlook.com>
2024-09-27 21:01:42 -0700

    Add better behavior for suspense in decklist view (#131)

--- client/src/components/fallback.tsx
+++ client/src/components/fallback.tsx
@@ -1,9 +1,22 @@
 import FireIcon from "@heroicons/react/24/solid/FireIcon";
+import cn from "classnames";
 import { Navigation } from "./navigation";
 
-export function LoadingIcon() {
+export function LoadingIcon({
+  padding = true,
+  className,
+}: {
+  padding?: boolean;
+  className?: string;
+}) {
   return (
-    <div className="flex w-full justify-center pt-24 text-white">
+    <div
+      className={cn(
+        "flex w-full justify-center  text-white",
+        padding && "pt-24",
+        className,
+      )}
+    >
       <FireIcon className="h-12 w-12 animate-pulse" />
     </div>
   );
