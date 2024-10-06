7fd76446981ffed275bf6b4f18609298b16b4033
Ryan Delaney <rrdelaney@outlook.com>
2024-10-06 14:49:46 -0700

    Remove `client_v1` directory and move `client` files into root

--- client_v1/src/contexts/ThemeContext.jsx
+++
@@ -1,9 +0,0 @@
-
-import { createContext } from "react";
-
-const ThemeContext = createContext({ theme: false, toggleTheme: () => {}, setTheme: () => {} });
-
-export const ThemeContextProvider = ThemeContext.Provider
-export const ThemeContextConsumer = ThemeContext.Consumer
-
-export default  ThemeContext;
