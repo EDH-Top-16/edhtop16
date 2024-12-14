0d45152036aea2e0406c677e1d58c67cf112d3ad
Ryan Delaney <rrdelaney@outlook.com>
2024-12-14 09:59:49 -0800

    Add color id and min enties filtering (#189)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,16 @@ import { NextSeo } from "next-seo";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { PropsWithChildren, useCallback, useMemo } from "react";
-import { graphql, useFragment, usePreloadedQuery } from "react-relay";
+import {
+  graphql,
+  useFragment,
+  usePaginationFragment,
+  usePreloadedQuery,
+} from "react-relay";
 import { RelayProps, withRelay } from "relay-nextjs";
 import { ColorIdentity } from "../assets/icons/colors";
 import { Card } from "../components/card";
+import { ColorSelection } from "../components/color_selection";
 import { Footer } from "../components/footer";
 import { Navigation } from "../components/navigation";
 import { Select } from "../components/select";
@@ -18,6 +24,9 @@ import {
   TimePeriod,
 } from "../queries/__generated__/pages_CommandersQuery.graphql";
 import { pages_TopCommandersCard$key } from "../queries/__generated__/pages_TopCommandersCard.graphql";
+import { pages_topCommanders$key } from "../queries/__generated__/pages_topCommanders.graphql";
+import { TopCommandersQuery } from "../queries/__generated__/TopCommandersQuery.graphql";
+import { LoadingIcon } from "../components/fallback";
 
 function TopCommandersCard({
   secondaryStatistic,
@@ -37,6 +46,7 @@ function TopCommandersCard({
           conversionRate
           topCuts
           count
+          metaShare
         }
       }
     `,
@@ -44,14 +54,18 @@ function TopCommandersCard({
   );
 
   const commanderStats = useMemo(() => {
-    const stats = [
-      `Conversion Rate: ${formatPercent(commander.stats.conversionRate)}`,
-    ];
+    const stats: string[] = [];
 
     if (secondaryStatistic === "count") {
-      stats.push(`Entries: ${commander.stats.count}`);
+      stats.push(
+        `Meta Share: ${formatPercent(commander.stats.metaShare)}`,
+        `Entries: ${commander.stats.count}`,
+      );
     } else if (secondaryStatistic === "topCuts") {
-      stats.push(`Top Cuts: ${commander.stats.topCuts}`);
+      stats.push(
+        `Conversion Rate: ${formatPercent(commander.stats.conversionRate)}`,
+        `Top Cuts: ${commander.stats.topCuts}`,
+      );
     }
 
     return stats.join(" / ");
@@ -82,12 +96,16 @@ function TopCommandersCard({
 function CommandersPageShell({
   sortBy,
   timePeriod,
+  colorId,
+  minEntries,
   onUpdateQueryParam,
   children,
 }: PropsWithChildren<{
+  colorId: string;
+  minEntries: number;
   sortBy: CommandersSortBy;
   timePeriod: TimePeriod;
-  onUpdateQueryParam?: (key: string, value: string) => void;
+  onUpdateQueryParam?: (key: string, value: string | null) => void;
 }>) {
   return (
     <>
@@ -98,10 +116,19 @@ function CommandersPageShell({
       />
 
       <div className="mx-auto mt-8 w-full max-w-screen-xl px-8">
-        <div className="mb-8 flex flex-col space-y-4 md:flex-row md:items-end md:space-y-0">
-          <h1 className="flex-1 font-title text-5xl font-extrabold text-white">
-            cEDH Metagame Breakdown
-          </h1>
+        <h1 className="mb-8 flex-1 text-center font-title text-5xl font-extrabold text-white md:mb-0 md:text-left">
+          cEDH Metagame Breakdown
+        </h1>
+
+        <div className="mb-8 flex flex-col items-center space-y-4 md:flex-row md:items-end">
+          <div className="flex-1">
+            <ColorSelection
+              selected={colorId}
+              onChange={(value) => {
+                onUpdateQueryParam?.("colorId", value || null);
+              }}
+            />
+          </div>
 
           <div className="flex space-x-4">
             <Select
@@ -132,6 +159,21 @@ function CommandersPageShell({
               <option value="ONE_YEAR">1 Year</option>
               <option value="ALL_TIME">All Time</option>
             </Select>
+
+            <Select
+              id="commanders-min-entries"
+              label="Min. Entries"
+              value={`${minEntries}`}
+              disabled={onUpdateQueryParam == null}
+              onChange={(value) => {
+                onUpdateQueryParam?.("minEntries", value);
+              }}
+            >
+              <option value="0">All Commanders</option>
+              <option value="20">20+</option>
+              <option value="60">60+</option>
+              <option value="120">120+</option>
+            </Select>
           </div>
         </div>
 
@@ -145,39 +187,71 @@ const CommandersQuery = graphql`
   query pages_CommandersQuery(
     $timePeriod: TimePeriod!
     $sortBy: CommandersSortBy!
+    $minEntries: Int!
+    $colorId: String
   ) {
-    commanders(first: 48, timePeriod: $timePeriod, sortBy: $sortBy) {
-      edges {
-        node {
-          id
-          ...pages_TopCommandersCard
-        }
-      }
-    }
+    ...pages_topCommanders
   }
 `;
 
 function Commanders({ preloadedQuery }: RelayProps<{}, pages_CommandersQuery>) {
-  const { commanders } = usePreloadedQuery(CommandersQuery, preloadedQuery);
+  const query = usePreloadedQuery(CommandersQuery, preloadedQuery);
 
   const router = useRouter();
   const setQueryVariable = useCallback(
-    (key: string, value: string) => {
+    (key: string, value: string | null) => {
       const nextUrl = new URL(window.location.href);
-      nextUrl.searchParams.set(key, value);
+      if (value == null) {
+        nextUrl.searchParams.delete(key);
+      } else {
+        nextUrl.searchParams.set(key, value);
+      }
       router.replace(nextUrl, undefined, { shallow: true, scroll: false });
     },
     [router],
+  );
+
+  const { data, loadNext, isLoadingNext, hasNext } = usePaginationFragment<
+    TopCommandersQuery,
+    pages_topCommanders$key
+  >(
+    graphql`
+      fragment pages_topCommanders on Query
+      @argumentDefinitions(
+        cursor: { type: "String" }
+        count: { type: "Int", defaultValue: 48 }
+      )
+      @refetchable(queryName: "TopCommandersQuery") {
+        commanders(
+          first: $count
+          after: $cursor
+          timePeriod: $timePeriod
+          sortBy: $sortBy
+          colorId: $colorId
+          minEntries: $minEntries
+        ) @connection(key: "pages__commanders") {
+          edges {
+            node {
+              id
+              ...pages_TopCommandersCard
+            }
+          }
+        }
+      }
+    `,
+    query,
   );
 
   return (
     <CommandersPageShell
       sortBy={preloadedQuery.variables.sortBy}
       timePeriod={preloadedQuery.variables.timePeriod}
+      colorId={preloadedQuery.variables.colorId || ""}
+      minEntries={preloadedQuery.variables.minEntries}
       onUpdateQueryParam={setQueryVariable}
     >
       <div className="grid w-fit grid-cols-1 gap-4 pb-4 md:grid-cols-2 xl:grid-cols-3">
-        {commanders.edges.map(({ node }) => (
+        {data.commanders.edges.map(({ node }) => (
           <TopCommandersCard
             key={node.id}
             commander={node}
@@ -190,6 +264,19 @@ function Commanders({ preloadedQuery }: RelayProps<{}, pages_CommandersQuery>) {
         ))}
       </div>
 
+      {isLoadingNext ? (
+        <LoadingIcon padding={false} className="self-center" />
+      ) : hasNext ? (
+        <button
+          className="inset-shadow-sm mx-auto flex justify-center self-center rounded-md bg-[#312d5a] px-4 py-2 font-title text-sm text-white shadow-md"
+          onClick={() => {
+            loadNext(48);
+          }}
+        >
+          Load More
+        </button>
+      ) : null}
+
       <Footer />
     </CommandersPageShell>
   );
@@ -197,21 +284,57 @@ function Commanders({ preloadedQuery }: RelayProps<{}, pages_CommandersQuery>) {
 
 function CommandersPagePlaceholder() {
   const router = useRouter();
+  const setQueryVariable = useCallback(
+    (key: string, value: string | null) => {
+      const nextUrl = new URL(window.location.href);
+      if (value == null) {
+        nextUrl.searchParams.delete(key);
+      } else {
+        nextUrl.searchParams.set(key, value);
+      }
+      router.replace(nextUrl, undefined, { shallow: true, scroll: false });
+    },
+    [router],
+  );
+
+  const sortBy =
+    (router.query.sortBy as CommandersSortBy | undefined) ?? "CONVERSION";
+  const timePeriod =
+    (router.query.timePeriod as TimePeriod | undefined) ?? "SIX_MONTHS";
+  const colorId = (router.query.colorId as string | undefined) || "";
+  const minEntries =
+    typeof router.query.minEntries === "string"
+      ? Number(router.query.minEntries)
+      : defaultMinEntriesForTimePeriod(timePeriod);
 
   return (
     <CommandersPageShell
-      sortBy={
-        (router.query.sortBy as CommandersSortBy | undefined) ?? "CONVERSION"
-      }
-      timePeriod={
-        (router.query.timePeriod as TimePeriod | undefined) ?? "SIX_MONTHS"
-      }
+      sortBy={sortBy}
+      timePeriod={timePeriod}
+      colorId={colorId}
+      minEntries={minEntries}
+      onUpdateQueryParam={setQueryVariable}
     >
       <div className="flex w-full justify-center pt-24 text-white">
         <FireIcon className="h-12 w-12 animate-pulse" />
       </div>
     </CommandersPageShell>
   );
+}
+
+function defaultMinEntriesForTimePeriod(timePeriod: TimePeriod): number {
+  switch (timePeriod) {
+    case "ALL_TIME":
+      return 120;
+    case "ONE_YEAR":
+      return 120;
+    case "SIX_MONTHS":
+      return 120;
+    case "THREE_MONTHS":
+      return 60;
+    default:
+      return 20;
+  }
 }
 
 export default withRelay(Commanders, CommandersQuery, {
@@ -225,9 +348,14 @@ export default withRelay(Commanders, CommandersQuery, {
     return createServerEnvironment();
   },
   variablesFromContext: (ctx) => {
-    return {
-      timePeriod: (ctx.query.timePeriod as TimePeriod) ?? "SIX_MONTHS",
-      sortBy: (ctx.query.sortBy as CommandersSortBy) ?? "CONVERSION",
-    };
+    const timePeriod = (ctx.query.timePeriod as TimePeriod) ?? "SIX_MONTHS";
+    const sortBy = (ctx.query.sortBy as CommandersSortBy) ?? "CONVERSION";
+    const colorId = ctx.query.colorId as string | undefined;
+    const minEntries =
+      typeof ctx.query.minEntries === "string"
+        ? Number(ctx.query.minEntries)
+        : defaultMinEntriesForTimePeriod(timePeriod);
+
+    return { timePeriod, sortBy, colorId, minEntries };
   },
 });
