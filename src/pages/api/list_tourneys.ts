66cdb65e646a0d98afdc359757ff74c7d9999efc
Ryan Delaney <rrdelaney@outlook.com>
2025-01-11 10:55:46 -0800

    Remove V1 API

--- src/pages/api/list_tourneys.ts
+++
@@ -1,4 +0,0 @@
-import { requireDeprecationAcknowledgement } from "../../lib/server/deprecation";
-import { listTourneysApi } from "../../lib/server/legacy_api";
-
-export default requireDeprecationAcknowledgement(listTourneysApi);
