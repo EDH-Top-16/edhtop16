35e0e7db60b9005454db90c60ac184b5de1739ce
Ryan Delaney <rrdelaney@outlook.com>
2025-05-26 20:44:05 -0700

    Actually fix label styles on mobile (#261)

--- src/pages/tournament/[TID].tsx
+++ src/pages/tournament/[TID].tsx
@@ -69,10 +69,10 @@ function EntryCard({
   }
 
   const entryNameNode = (
-    <span className="flex items-center">
+    <span className="z-10 flex items-center">
       {entryName}
       {entry.player?.isKnownCheater && (
-        <span className="z-10 ml-2 rounded-full bg-red-600 px-2 py-1 text-xs uppercase">
+        <span className="ml-2 rounded-full bg-red-600 px-2 py-1 text-xs uppercase">
           Cheater
         </span>
       )}
