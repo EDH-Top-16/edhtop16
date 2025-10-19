90a55ee0c113673a8d5fff5c93f97ea6f00fa14c
Ryan Delaney <rrdelaney@outlook.com>
2025-10-18 22:43:52 -0700

    Update Pastoria version

--- src/lib/server/context.ts
+++ src/lib/server/context.ts
@@ -1,23 +1,4 @@
-import DataLoader from 'dataloader';
+import {PastoriaRootContext} from 'pastoria-runtime/server';
 
 /** @gqlContext */
-export class Context {
-  constructor() {}
-
-  private readonly DERIVED_CACHE = new Map<string, unknown>();
-
-  derived = <T>(key: string, make: () => T): T => {
-    if (!this.DERIVED_CACHE.has(key)) {
-      this.DERIVED_CACHE.set(key, make());
-    }
-
-    return this.DERIVED_CACHE.get(key) as T;
-  };
-
-  loader = <K, V>(
-    key: string,
-    batchLoadFn: DataLoader.BatchLoadFn<K, V>,
-  ): DataLoader<K, V> => {
-    return this.derived(key, () => new DataLoader(batchLoadFn));
-  };
-}
+export class Context extends PastoriaRootContext {}
