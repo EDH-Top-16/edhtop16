4c32796ab8dc53a11ca3681868a4f19139b118b0
Ryan Delaney <rrdelaney@outlook.com>
2025-12-09 21:16:34 -0800

    Set up GA

--- src/_app.tsx
+++ src/_app.tsx
@@ -3,6 +3,13 @@ import {PropsWithChildren, Suspense} from 'react';
 
 import './globals.css';
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
 /** @appRoot */
 export function App({children}: PropsWithChildren<{}>) {
   return (
@@ -13,6 +20,24 @@ export function App({children}: PropsWithChildren<{}>) {
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
       />
+
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-51DLXH804W"
+        onLoad={() => {
+          window.dataLayer = window.dataLayer || [];
+
+          function gtag(...args: any): void;
+          function gtag() {
+            // eslint-disable-next-line prefer-rest-params
+            window.dataLayer.push(arguments);
+          }
+
+          gtag('js', new Date());
+          gtag('config', 'G-51DLXH804W');
+        }}
+      />
+
       <Suspense fallback={<Edhtop16Fallback />}>
         <main className="relative min-h-screen bg-[#514f86]">{children}</main>
       </Suspense>
