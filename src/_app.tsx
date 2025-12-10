ecc1821c26ef04a7e797621ad2651e889f1420ca
Ryan Delaney <rrdelaney@outlook.com>
2025-12-09 21:27:22 -0800

    Set up GA (#366)
    * Set up GA
    
    * Fix

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,10 +1,26 @@
 import {Edhtop16Fallback} from '#src/components/fallback';
-import {PropsWithChildren, Suspense} from 'react';
+import {PropsWithChildren, Suspense, useEffect} from 'react';
 
 import './globals.css';
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
 /** @appRoot */
 export function App({children}: PropsWithChildren<{}>) {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(...args: any): void {
+      window.dataLayer.push(args);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-51DLXH804W');
+  }, []);
+
   return (
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -13,6 +29,12 @@ export function App({children}: PropsWithChildren<{}>) {
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
       />
+
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-51DLXH804W"
+      />
+
       <Suspense fallback={<Edhtop16Fallback />}>
         <main className="relative min-h-screen bg-[#514f86]">{children}</main>
       </Suspense>
