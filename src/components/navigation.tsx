59899f52935bb378bc6f87c9936fca7d82c54e82
Ryan Delaney <rrdelaney@outlook.com>
2026-04-03 08:38:16 -0700

    Remove leaderboard/Rankings link from navigation (#427)

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -16,7 +16,7 @@ export function Navigation({
   }, []);
 
   return (
-    <nav className="font-title sticky top-0 z-20 grid w-full grid-cols-[auto_auto_auto_auto_1fr] items-center gap-x-6 gap-y-3 bg-[#312d5a] px-4 py-3 text-white sm:grid-cols-[auto_auto_auto_auto_auto_1fr] md:px-8">
+    <nav className="font-title sticky top-0 z-20 grid w-full grid-cols-[auto_auto_auto_1fr] items-center gap-x-6 gap-y-3 bg-[#312d5a] px-4 py-3 text-white sm:grid-cols-[auto_auto_auto_auto_1fr] md:px-8">
       <Link href="/" className="text-xl font-black">
         EDHTop16
       </Link>
@@ -42,13 +42,6 @@ export function Navigation({
         Staples
       </Link>
 
-      <Link
-        href="/leaderboard"
-        className="text-xs underline decoration-transparent transition-colors hover:decoration-inherit md:text-sm"
-      >
-        Rankings
-      </Link>
-
       <button
         className="block justify-self-end md:hidden"
         onClick={toggleSearch}
@@ -62,7 +55,7 @@ export function Navigation({
 
       <div
         className={cn(
-          'col-span-5 justify-end md:col-span-1 md:flex',
+          'col-span-4 justify-end md:col-span-1 md:flex',
           mobileSearchOpen ? 'flex' : 'hidden',
         )}
       >
